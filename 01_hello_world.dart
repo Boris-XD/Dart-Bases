e0c0e751fad6a5f295b759dd7a5e7a826276f242
Boris Sea <seaborisrc@gmail.com>
2023-08-09 22:59:49 -0400

    Hello world

---
+++ 01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+   // var myname = 'Fernando';
+  // late final myName;
+  // String myName = 'Boris';
+  
+  const myName = 'Boris';
+  
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Sumando 1 + 1 = ${ 1 + 1 }');
+}
